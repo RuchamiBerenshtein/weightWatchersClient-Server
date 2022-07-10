4969193bc8f68138fe59db115e6a9ebe9449f1ed
RuchamiBerenshtein <r9743740@gmail.com>
2022-07-10 15:05:54 +0300

    all meeting server is work!

--- server/Controllers/meetingController.js
+++ server/Controllers/meetingController.js
@@ -1,6 +1,6 @@
 const meetingService = require('../services/meetingService');
 
-module.exports.getAllMeeting = async function (req, res) {
+const getAllMeeting = async (req, res) => {
     try {
         const users = await meetingService.getAll();
         res.send(users);
@@ -9,29 +9,31 @@ module.exports.getAllMeeting = async function (req, res) {
         res.status(500).json({
             massage: `reading from json was failed ${err}`
         })
-    } 
+    }
 }
 
-module.exports.getMeetingById = async function (req, res) {
-        const id = parseInt(req.params.id);
-        try {
-            const meeting = await meetingService.getMeetingById(id);
-            res.send(meeting);
-        }
-        catch (err) {
-            res.status(500).json({
-                massage: `reading from json was failed ${err}`
-            })
-        }
+const getMeetingById = async (req, res) => {
+    const id = parseInt(req.params.id);
+    try {
+        const meeting = await meetingService.getMeetingById(id);
+        res.status(200).json({
+            meeting
+        })
+    }
+    catch (err) {
+        res.status(500).json({
+            massage: `reading from json was failed ${err}`
+        })
+    }
 }
 
-module.exports.addMeeting = async function (req, res) {
- 
+const addMeeting = async (req, res) => {
+
     try {
         if (req.body) {
-            const  meeting  = req.body.weights;
-            const created = await meetingService.addMeeting(meeting);
-            console.log(created);
+            const date = req.body.date;
+            const weights = req.body.weights;
+            const created = await meetingService.addMeeting(weights, date);
             res.send(created);
         }
         else {
@@ -47,11 +49,51 @@ module.exports.addMeeting = async function (req, res) {
     }
 }
 
-module.exports.updateMeeting = async function (req, res) {
-    
+const updateMeeting = async (req, res) => {
+    const id = parseInt(req.params.id);
+    const { meeting } = req.body;
+    const { weights } = req.body;
+
+    if (id !== meeting.id) {
+        res.status(400).json({
+            massage: 'there is not a same meeting'
+        });
+    }
+
+    const existingMeet = meetingService.getMeetingById(id);
+    if (!existingMeet) {
+        res.status(400).json({
+            massage: 'meeting is not find'
+        })
+    }
+
+    meetingService.updateMeeting(meeting, weights);
+    res.status(200).json({
+        massage: `user ${meeting.id} is updated`
+    })
 }
 
-module.exports.deleteMeeting = async function (req, res) {
-    
+const deleteMeeting = async (req, res) => {
+    const id = parseInt(req.params.id);
+
+    try {
+        const deleteMeeting = await meetingService.deleteMeeting(id)
+        res.status(200).json({
+            massage: `meeting ${deleteMeeting + 1} was deleted`
+        })
+
+    }
+    catch (err) {
+        res.status(500).json({
+            massage: `writing to json was failed ${err}`
+        })
+    }
 }
 
+module.exports = {
+    getAllMeeting,
+    getMeetingById,
+    addMeeting,
+    updateMeeting,
+    deleteMeeting
+}
