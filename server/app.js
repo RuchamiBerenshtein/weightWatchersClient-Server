109f44b175404f05eac8490524038027ea7af5fc
chaniYehudit <p0548453160@gmail.com>
2022-07-11 20:17:56 +0300

    swagger file

--- server/app.js
+++ server/app.js
@@ -5,17 +5,25 @@ const user = require('./Routs/userRout');
 const diary = require('./Routs/diaryRout');
 const meeting = require('./Routs/meetingRout');
 const account = require('./Routs/accountRout');
-
+const swaggerUi = require('swagger-ui-express'),
+swaggerDocument = require('./swagger.json');
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
+
 app.use('/User', user);
 app.use('/Diary', diary);
 app.use('/Account', account);
 app.use('/Meeting', meeting);
+
+app.use(
+  '/api-docs',
+  swaggerUi.serve, 
+  swaggerUi.setup(swaggerDocument)
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port} :)`)
