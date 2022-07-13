5540932946949fcf9dbb5c83c0173706dbf78bcd
chaniYehudit <p0548453160@gmail.com>
2022-07-13 15:50:17 +0300

    aaa

--- server/services/accountService.js
+++ server/services/accountService.js
@@ -3,12 +3,14 @@ const jsonData = fs.readFileSync('data/users.json');
 const data = JSON.parse(jsonData);
 
 const login = async (email) => {
-debugger
-//     const user =await data.manager.find(user => user.email === email);
-//    if (!user){
-    const user =await data.users.find(user => user.email === email);
-    
-    return await user;
+
+    return await newFunction();
+
+    async function newFunction() {
+        const user = await data.users.find(user => user.email === email);
+
+        return await user;
+    }
 }
 
 module.exports ={login} 
