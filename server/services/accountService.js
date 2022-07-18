0eff40d5afdbf76cb05790a6bd816a6441d53b1d
chaniYehudit <p0548453160@gmail.com>
2022-07-18 09:52:24 +0300

    aaa

--- server/services/accountService.js
+++ server/services/accountService.js
@@ -4,10 +4,7 @@ const data = JSON.parse(jsonData);
 
 const login = async (email) => {
 
-    const user =await data.manager.find(user => user.email === email);
-   if (!user){
     const user =await data.users.find(user => user.email === email);
-    }
     return await user;
 }
 
