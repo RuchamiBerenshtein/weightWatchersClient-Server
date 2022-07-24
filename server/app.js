67fdb78323796dc9d807ac0c441b9b42be0ee2e6
chaniYehudit <p0548453160@gmail.com>
2022-07-24 12:58:05 +0300

    add login

--- server/app.js
+++ server/app.js
@@ -8,18 +8,42 @@ const meeting = require('./Routs/meetingRout');
 const account = require('./Routs/accountRout');
 const swaggerUi = require('swagger-ui-express'),
 swaggerDocument = require('./swagger.json');
+const { auth } = require('express-openid-connect');
 const port = 3000;
+const { requiresAuth } = require('express-openid-connect');
+
 
 db.connect();
 
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3000',
+  clientID: 'PUXVNcvCQ5hIfUWZvFrXayrZIEAEBSDk',
+  issuerBaseURL: 'https://dev-wvph8f3k.us.auth0.com'
+};
+
+app.use(auth(config));
+
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
-
-app.use('/user', user);
-app.use('/diary', diary);
-app.use('/account', account);
-app.use('/meeting', meeting);
+app.use('/user', requiresAuth(), user);
+app.use('/diary', requiresAuth(), diary);
+app.use('/account',requiresAuth(),  account);
+app.use('/meeting', requiresAuth(), meeting);
+//app.use('/user', user);
+// app.use('/diary', diary);
+// app.use('/account', account);
+// app.use('/meeting', meeting);
 
 app.use(
   '/api-docs',
